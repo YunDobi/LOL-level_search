37fdb008b0c86c667e1ab5ea478affe2a5e1adcc
Yun <88572626+YunDobi@users.noreply.github.com>
2021-09-24 20:44:17 -0700

    Delete example.py

--- example.py
+++
@@ -1,7 +0,0 @@
-from riotwatcher import LolWatcher, ApiError
-
-lol_watcher = LolWatcher('RGAPI-668cb7b7-972a-4669-9e8c-864245d5f8b4')
-my_region = 'na1'
-
-me = lol_watcher.summoner.by_name(my_region, 'No candy for u')
-print(me)
